f87b794b3fd07e8bc9c0d47f20bc189ddae72078
accessgames <83598795+accessgames@users.noreply.github.com>
2024-05-07 09:36:17 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-
-import defineConfig from 'vite';
+{
+import defineConfig } from 'vite';
 import preact from '@preact/preset-vite'; 
 import { ViteMinifyPlugin } from 'vite-plugin-minify';
 import { createBareServer } from '@tomphttp/bare-server-node';
